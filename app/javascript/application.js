a23db2088ae61a470b1245a73658bb3918b8d831
Mark Streltsin <streltsin.mark@gmail.com>
2024-03-02 13:15:38 +0200

    Added a menu nav-bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import '@doabit/semantic-ui-sass'
+import * as jquery from "jquery"
+import "semantic-ui"
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown()
+});
