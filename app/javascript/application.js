c942b74a745a4d9a01ddb1e163bb0968543c9803
Mark Streltsin <streltsin.mark@gmail.com>
2024-03-02 21:30:44 +0200

    Enable flash messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,4 +7,8 @@ import "semantic-ui"
 
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown()
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+      });
 });
+
