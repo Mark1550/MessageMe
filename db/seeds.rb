67d57e5e7bef2098524b4303201c0360a9282814
Mark Streltsin <streltsin.mark@gmail.com>
2024-03-02 20:23:57 +0200

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,8 @@ User.create(username: "Frodo", password: "1234")
 User.create(username: "Jonsnow", password: "1234")
 User.create(username: "Arya", password: "1234")
 User.create(username: "Gandalf", password: "1234")
+
+Message.create(body: "Mark's Message", user_id: 1)
+Message.create(body: "Evgeny's Message", user_id: 2)
+Message.create(body: "Frodo's Message", user_id: 3)
+Message.create(body: "Jonsow's Message", user_id: 4)
